5cbafec459a67d592638c8858446f6f3e33614ba
Daniel Gibbs <daniel@danielgibbs.name>
2012-12-19 00:13:20 +1300

    Generates RSS feeds now. Works well enough for my purposes.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-trademe-rss
+TradeMe RSS
 ===========
 
 Provides RSS feeds for search results on the online auction site TradeMe.
+Very basic, and probably quite crude.
+
+To use, just put the files on a webserver, and make sure it knows how to execute python scripts.
--- rss.py
+++ rss.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import sys
+import json
+import cgi
+import time
+
+from urllib2 import urlopen
+from urllib import quote_plus
+from urllib import unquote_plus
 
 def create_rss(search_url):
 	# Check for valid TradeMe search URL.
 	if not search_url.startswith("http://www.trademe.co.nz/Browse/SearchResults.aspx?"):
+		print "Content-type: text/html"
+		print
 		print "Invalid URL: %s" % search_url
 		return
 	search = search_url[search_url.find("?")+1:]
@@ -14,10 +23,15 @@ def create_rss(search_url):
 	search_params = dict(x.split("=") for x in search.split("&"))
 
 	# Build API URL.
-	api_url = "http://api.trademe.co.nz/v1/Search/General.json?"
+	api_url = "http://api.trademe.co.nz/v1/Search/General.json?expired=false"
 
 	if "searchString" in search_params:
 		api_url += "&search_string=" + search_params["searchString"]
+	else:
+		print "Content-type: text/html"
+		print
+		print "No query string found."
+		return
 
 	if "searchregion" in search_params:
 		api_url += "&region=" + search_params["searchregion"]
@@ -38,12 +52,87 @@ def create_rss(search_url):
 	else:
 		api_url += "&pay=All"
 
-	# TODO: Sort order
+	if "sort_order" in search_params:
+		if search_params["sort_order"] == "feature_first":
+			api_url += "&sort_order=Default"
+		elif search_params["sort_order"] == "price_asc":
+			api_url += "&sort_order=PriceAsc"
+		elif search_params["sort_order"] == "price_desc":
+			api_url += "&sort_order=PriceDesc"
+		elif search_params["sort_order"] == "buynow_asc":
+			api_url += "&sort_order=BuyNowAsc"
+		elif search_params["sort_order"] == "buynow_desc":
+			api_url += "&sort_order=BuyNowDesc"
+		elif search_params["sort_order"] == "bids_asc":
+			api_url += "&sort_order=BidsMost"
+		elif search_params["sort_order"] == "expiry_desc":
+			api_url += "&sort_order=ExpiryDesc"
+		elif search_params["sort_order"] == "expiry_asc":
+			api_url += "&sort_order=ExpiryAsc"
+		elif search_params["sort_order"] == "title_asc":
+			api_url += "&sort_order=TitleAsc"
 
-	# Print out API URL.
-	print api_url
+	search_result = json.load(urlopen(api_url))
 
-for arg in sys.argv[1:]:
-	create_rss(arg)
+	print "Content-Type: application/xml"
+	print
+	print "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+	print "<rss version=\"2.0\">"
+	print "<channel>"
+	print "<title>TradeMe search for %s</title>" % unquote_plus(search_params["searchString"])
+	print "<link>%s</link>" % quote_plus(search_url)
+	print "<description>TradeMe search for %s</description>" % search_params["searchString"]
+	print "<language>en-nz</language>"
+	print "<image>"
+	print "<url>http://www.trademe.co.nz/images/iphonelogo.png</url>"
+	print "<title>TradeMe Logo</title>"
+	print "<link>%s</link>" % quote_plus(search_url)
+	print "</image>"
+
+	for item in search_result["List"]:
+		print "<item>"
+		print "<title>%s</title>" % cgi.escape(item["Title"])
+		print "<link>http://www.trademe.co.nz/Browse/Listing.aspx?id=%s</link>" % item["ListingId"]
+		print "<description>"
+		if "PictureHref" in item:
+			print cgi.escape("<img src=\"%s\" title=\"%s\" />" % (item["PictureHref"], item["Title"]))
+		print cgi.escape("<p>")
+		if "Suburb" in item and "Region" in item:
+			print cgi.escape("<strong>Location: </strong>%s, %s<br />" % (item["Suburb"], item["Region"]))
+		print cgi.escape("<strong>Closes: </strong>%s<br />" % time.strftime("%a, %d %b %Y %I:%M:%S %p", time.localtime(float(item["EndDate"][6:-2])/1000)))
+		print cgi.escape("</p>")
+		print cgi.escape("<p>");
+		if "StartPrice" in item:
+			print cgi.escape("<strong>Start Price: </strong>$%.2f<br />" % item["StartPrice"])
+		if "BuyNowPrice" in item:
+			print cgi.escape("<strong>Buy Now Price: </strong>$%.2f<br />" % item["BuyNowPrice"])
+		if "MaxBidAmount" in item:
+			print cgi.escape("<strong>Current Bid: </strong>$%.2f<br />" % item["MaxBidAmount"])
+		print cgi.escape("</p>");
+
+		if "IsReserveMet" in item:
+			if item["IsReserveMet"]:
+				print cgi.escape("<strong>Reserve Met</strong><br />")
+			else:
+				if not "HasReserve" in item or not item["HasReserve"]:
+					print cgi.escape("<strong>No Reserve</strong><br />")
+				else:
+					print cgi.escape("<strong>Reserve Not Met</strong><br />")
+		else:
+			if not "HasReserve" in item or not item["HasReserve"]:
+				print cgi.escape("<strong>No Reserve</strong><br />")
 
 
+		print "</description>"
+		print "<pubDate>%s</pubDate>" % time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(float(item["StartDate"][6:-2])/1000))
+		if "CategoryName" in item:
+			print "<category>%s</category>" % cgi.escape(item["CategoryName"])
+		print "<guid>%s</guid>" % item["ListingId"]
+		print "</item>"
+
+	print "</channel>"
+	print "</rss>"
+
+form = cgi.FieldStorage()	
+search_url = form.getfirst('url', '')
+create_rss(search_url)
