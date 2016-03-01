b2248c7250420d2da327730f641162ba8af73537
Daniel Gibbs <daniel@danielgibbs.name>
2016-03-01 20:02:53 +1300

    Add Python 3 compatibility

--- rss.py
+++ rss.py
@@ -14,8 +14,7 @@ import sys
 import json
 import cgi
 import time
-
-from urllib import quote_plus, unquote_plus
+from urllib.parse import quote_plus, unquote_plus
 from hashlib import md5
 
 import requests
@@ -34,8 +33,8 @@ def create_rss(search_url, dont_show_relistings):
 	# Check for valid TradeMe search URL.
 	# TODO: Support vehicle, job, house search etc.
 	if not re.match("^http[s]?://www\.trademe\.co\.nz/Browse/SearchResults\.aspx\?.*", search_url):
-		print "Location: index.html"
-		print
+		print("Location: index.html")
+		print()
 		return
 	search = search_url[search_url.find("?")+1:]
 
@@ -51,8 +50,8 @@ def create_rss(search_url, dont_show_relistings):
 	if "searchString" in search_params:
 		api_url += "&search_string=" + search_params["searchString"]
 	else:
-		print "Location: index.html"
-		print
+		print("Location: index.html")
+		print()
 		return
 
 	if "searchregion" in search_params:
@@ -99,70 +98,70 @@ def create_rss(search_url, dont_show_relistings):
 	# TODO: Check status of the API call result.
 
 	# Output the RSS header and feed description.
-	print "Content-Type: application/xml"
-	print
-	print "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
-	print "<rss version=\"2.0\">"
-	print "<channel>"
-	print "<title>TradeMe search for %s</title>" % unquote_plus(search_params["searchString"])
-	print "<link>%s</link>" % quote_plus(search_url)
-	print "<description>TradeMe search for %s</description>" % search_params["searchString"]
-	print "<language>en-nz</language>"
-	print "<image>"
-	print "<url>http://www.trademe.co.nz/images/iphonelogo.png</url>"
-	print "<title>TradeMe Logo</title>"
-	print "<link>%s</link>" % quote_plus(search_url)
-	print "</image>"
+	print("Content-Type: application/xml")
+	print()
+	print("<?xml version=\"1.0\" encoding=\"utf-8\"?>")
+	print("<rss version=\"2.0\">")
+	print("<channel>")
+	print("<title>TradeMe search for %s</title>" % unquote_plus(search_params["searchString"]))
+	print("<link>%s</link>" % quote_plus(search_url))
+	print("<description>TradeMe search for %s</description>" % search_params["searchString"])
+	print("<language>en-nz</language>")
+	print("<image>")
+	print("<url>http://www.trademe.co.nz/images/iphonelogo.png</url>")
+	print("<title>TradeMe Logo</title>")
+	print("<link>%s</link>" % quote_plus(search_url))
+	print("</image>")
 
 	# For each item in the search result, output the RSS feed entry.
 	try:
 		for item in search_result["List"]:
-			print "<item>"
-			print "<title>%s</title>" % cgi.escape(item["Title"])
-			print "<link>http://www.trademe.co.nz/Browse/Listing.aspx?id=%s</link>" % item["ListingId"]
-			print "<description>"
+			print("<item>")
+			print("<title>%s</title>" % cgi.escape(item["Title"]))
+			print("<link>http://www.trademe.co.nz/Browse/Listing.aspx?id=%s</link>" % item["ListingId"])
+			print("<description>")
 			if "PictureHref" in item:
-				print cgi.escape("<img src=\"%s\" title=\"%s\" />" % (item["PictureHref"], item["Title"]))
-			print cgi.escape("<p>")
+				print(cgi.escape("<img src=\"%s\" title=\"%s\" />" % (item["PictureHref"], item["Title"])))
+			print(cgi.escape("<p>"))
 			if "Suburb" in item and "Region" in item:
-				print cgi.escape("<strong>Location: </strong>%s, %s<br />" % (item["Suburb"], item["Region"]))
-			print cgi.escape("<strong>Closes: </strong>%s<br />" % time.strftime("%a, %d %b %Y %I:%M:%S %p", time.localtime(float(item["EndDate"][6:-2])/1000)))
-			print cgi.escape("</p>")
-			print cgi.escape("<p>");
+				print(cgi.escape("<strong>Location: </strong>%s, %s<br />" % (item["Suburb"], item["Region"])))
+			print(cgi.escape("<strong>Closes: </strong>%s<br />" % time.strftime("%a, %d %b %Y %I:%M:%S %p", time.localtime(float(item["EndDate"][6:-2])/1000))))
+			print(cgi.escape("</p>"))
+			print(cgi.escape("<p>"));
 			if "StartPrice" in item:
-				print cgi.escape("<strong>Start Price: </strong>$%.2f<br />" % item["StartPrice"])
+				print(cgi.escape("<strong>Start Price: </strong>$%.2f<br />" % item["StartPrice"]))
 			if "BuyNowPrice" in item:
-				print cgi.escape("<strong>Buy Now Price: </strong>$%.2f<br />" % item["BuyNowPrice"])
+				print(cgi.escape("<strong>Buy Now Price: </strong>$%.2f<br />" % item["BuyNowPrice"]))
 			if "MaxBidAmount" in item:
-				print cgi.escape("<strong>Current Bid: </strong>$%.2f<br />" % item["MaxBidAmount"])
-			print cgi.escape("</p>");
+				print(cgi.escape("<strong>Current Bid: </strong>$%.2f<br />" % item["MaxBidAmount"]))
+			print(cgi.escape("</p>"));
 
 			if "IsReserveMet" in item:
 				if item["IsReserveMet"]:
-					print cgi.escape("<strong>Reserve Met</strong><br />")
+					print(cgi.escape("<strong>Reserve Met</strong><br />"))
 				else:
 					if not "HasReserve" in item or not item["HasReserve"]:
-						print cgi.escape("<strong>No Reserve</strong><br />")
+						print(cgi.escape("<strong>No Reserve</strong><br />"))
 					else:
-						print cgi.escape("<strong>Reserve Not Met</strong><br />")
+						print(cgi.escape("<strong>Reserve Not Met</strong><br />"))
 			else:
 				if not "HasReserve" in item or not item["HasReserve"]:
-					print cgi.escape("<strong>No Reserve</strong><br />")
+					print(cgi.escape("<strong>No Reserve</strong><br />"))
 
-			print "</description>"
-			print "<pubDate>%s</pubDate>" % time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(float(item["StartDate"][6:-2])/1000))
+			print("</description>")
+			print("<pubDate>%s</pubDate>" % time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.localtime(float(item["StartDate"][6:-2])/1000)))
 			if "CategoryName" in item:
-				print "<category>%s</category>" % cgi.escape(item["CategoryName"])
+				print("<category>%s</category>" % cgi.escape(item["CategoryName"]))
 			if dont_show_relistings:
-				print "<guid>%s</guid>" % md5(item["Title"]).hexdigest()
+				print("<guid>%s</guid>" % md5(item["Title"]).hexdigest())
 			else:
-				print "<guid>%s</guid>" % item["ListingId"]
-			print "</item>"
+				print("<guid>%s</guid>" % item["ListingId"])
+			print("</item>")
 	except Exception as e:
 		sys.stderr.write("%s\n" % str(e))
 
-	print "</channel>"
-	print "</rss>"
+	print("</channel>")
+	print("</rss>")
 
 # Get the TradeMe search URL from the request and output an RSS feed.
 form = cgi.FieldStorage()
